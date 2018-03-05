68bf13f5203d6e2cb568b4d4f749151a34e95baf
Gleb Zhebrakov <glebzhebrakov@gmail.com>
2018-03-06 01:05:38 +0200

    cassandra

--- src/main/groovy/hme/poc/hmepoc/dto/TestMessage.groovy
+++ src/main/groovy/hme/poc/hmepoc/dto/TestMessage.groovy
@@ -1,12 +1,17 @@
 package hme.poc.hmepoc.dto
 
 import groovy.transform.Canonical
+import org.springframework.data.cassandra.core.mapping.PrimaryKey
+import org.springframework.data.cassandra.core.mapping.Table
 import org.springframework.data.redis.core.RedisHash
 
 @Canonical
 @RedisHash("TestMessage")
+@Table
 class TestMessage implements Serializable {
     String payload
+
+    @PrimaryKey
     String id
     long timestamp
 }
