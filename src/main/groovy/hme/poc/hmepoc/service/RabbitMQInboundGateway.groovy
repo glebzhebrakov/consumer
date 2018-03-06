7d3f43550c0f407453f41c2162a1db2c78825cb9
glib.zhebrakov <glib.zhebrakov@altexsoft.com>
2018-03-06 18:38:39 +0200

    rabbitmq

--- src/main/groovy/hme/poc/hmepoc/service/RabbitMQInboundGateway.groovy
+++ src/main/groovy/hme/poc/hmepoc/service/RabbitMQInboundGateway.groovy
@@ -1,5 +1,7 @@
 package hme.poc.hmepoc.service
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import hme.poc.hmepoc.dto.TestMessage
 import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Profile
@@ -10,15 +12,17 @@ import org.springframework.stereotype.Service
 class RabbitMQInboundGateway implements MessageConsumingPort {
 
     private final MessageStoragePort messageStoragePort
+    private final ObjectMapper objectMapper
 
     @Autowired
-    RabbitMQInboundGateway(final MessageStoragePort messageStoragePort) {
+    RabbitMQInboundGateway(final ObjectMapper objectMapper, final MessageStoragePort messageStoragePort) {
         this.messageStoragePort = messageStoragePort
+        this.objectMapper = objectMapper
     }
 
-    @RabbitListener(queues = '${rabbit.queue}')
+    @RabbitListener(queues = '${rabbit.queueName}')
     @Override
     void receive(byte[] message) {
-
+        messageStoragePort.save(objectMapper.readValue(message, TestMessage))
     }
 }
